5a2f6851e7c39c513884a04594f77d782edf131b
exclusive-and <exclusiveandgate@gmail.com>
2022-12-09 00:56:24 -0500

    Minor refactor changes; added imports for the future

--- src/Giskard/Syntax.hs
+++ src/Giskard/Syntax.hs
@@ -5,67 +5,84 @@
 -- Description  : Abstract Syntax of the Giskard Language.
 -----------------------------------------------------------
 module Giskard.Syntax
-    ( Judgement (..), Deduction (..)
-    , Term' (..), Binder (..), Term, Type
+    ( Judgement (..)
+    , Deduction (..)
+    , Term' (..), Binder (..)
+    , Term, Type
     ) where
 
-
--- |
--- Judgements are claims about types and terms with respect to their type.
---
-data Judgement
-    -- |
-    -- Claims the formation of a new type.
-    = JFormation Type
-
-    -- |
-    -- Claims that two types are definitionally equal.
-    | JTypesEqual Type Type
-
-    -- |
-    -- Claims the typing of a term.
-    | JType Term Type
-
-    -- |
-    -- Claims that two terms are definitionally equal.
-    | JEqual Term Term Type
+import              Control.DeepSeq (NFData)
+import              Data.IntMap (IntMap)
+import              Data.HashMap.Strict (HashMap)
+import qualified    Data.List as List
+import              Data.Text (Text)
 
 
 -- |
--- A deduction is a relationship of lists of claims. If we assume the claims
--- listed in the hypotheses, then we can deduce the claims listed as
--- conclusions.
+-- Judgements are claims about formation and equality of types, and
+-- the typing of terms and equality of terms.
 --
-data Deduction = Deduction
-    { hypotheses    :: [Judgement]
-    , conclusions   :: [Judgement]
+data Judgement aterm atype
+
+    -- | Claims the formation of a new type.
+    = JudgeTypeFormation atype
+
+    -- | Claims that two types are definitionally equal.
+    | JudgeTypeEquality atype atype
+
+    -- | Claims the typing of a term.
+    | JudgeType aterm atype
+
+    -- | Claims that two terms are definitionally equal.
+    | JudgeEqual aterm aterm atype
+
+    deriving Show
+
+-- |
+-- A deduction is a relationship of lists of claims. If we assume the
+-- claims listed in the hypotheses, then we can deduce the claims listed
+-- as conclusions.
+--
+data Deduction aterm atype = Deduction
+    { hypotheses  :: [Judgement aterm atype]
+    , conclusions :: [Judgement aterm atype]
     }
+    deriving Show
 
 
+-- |
+-- Algebra of typed terms.
+--
 data Term' name
-    = Var Int
+    = Var name
 
-    -- |
-    -- Bind a term of some type to a name in the body sub-term.
+    -- | Bind a term to a name in the body of the bind.
     | Bind
         name                    -- ^ Name to bind to.
         (Binder (Term' name))   -- ^ Binder data.
         (Term' name)            -- ^ Sub-term with bound term.
 
-    -- |
-    -- Apply one term to another.
+    -- | Apply one term to another.
     | App
         (Term' name)
         (Term' name)
+
+    deriving Show
 
 -- |
 -- Information about what and how exactly we're binding some term.
 --
 data Binder bindTy
-    = Lam bindTy
-    | Pi  bindTy bindTy
+    = Lam bindTy        -- ^ Bind with @\(x : A) -> y : B@
+    | Pi  bindTy bindTy -- ^ Bind with @Pi (x : A) -> y : B x@
     | Let bindTy bindTy
+    deriving Show
 
 
 type Term = Term' Name
 type Type = Term
+-- TEMPORARY
+type Name = Text
+
+
+
