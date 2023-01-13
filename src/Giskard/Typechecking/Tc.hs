b6d9465ef17f7f03ab7459471d059c91a64bea68
xand <xand@xandgate.com>
2023-01-12 21:32:12 -0500

    [WIP] Added reusable typechecker defns

---
+++ src/Giskard/Typechecking/Tc.hs
@@ -0,0 +1,159 @@
+
+{-# LANGUAGE UndecidableInstances #-}
+
+-----------------------------------------------------------
+-- |
+-- Module       : Giskard.Typechecking.Tc
+-- Description  : Some Reusable Type-checker Types and Classes
+-----------------------------------------------------------
+module Giskard.Typechecking.Tc where
+
+import              Giskard.Calculus.Term
+import              Giskard.Calculus.Pretty.Term
+import              Giskard.Calculus.SynEq
+
+import              Giskard.Names
+import              Giskard.Pretty
+
+import qualified    Data.Kind as Kind
+import              Data.Map (Map)
+import qualified    Data.Map as Map
+import              Data.Text (Text)
+
+
+type TcTerm theTc = Term' (TcPoint theTc)
+
+type TcType theTc = Type' (TcPoint theTc)
+
+-- |
+-- What kind of point should a typechecker use?
+-- 
+-- Examples:
+--  * Simply-Typed Lambda Calculus: @TcPoint STLC = Name@.
+--  * System F: @TcPoint SystemF = P@
+-- 
+data family TcPoint theTc :: Kind.Type
+
+-- |
+-- Which monad implements a typechecker?
+--
+data family TcMonad theTc :: Kind.Type -> Kind.Type
+
+
+-- |
+-- Type indices that are typecheckers.
+-- 
+class Typechecker theTc where
+    -- |
+    -- Infer the type of a term. Returns the term, since there
+    -- may be specializations or reductions applied.
+    infer
+        :: TcTerm theTc
+        -> TcMonad theTc (TcTerm theTc, TcType theTc)
+    
+    -- | Check the actual type of a term against an expected type.
+    check
+        :: TcTerm theTc
+        -> TcType theTc
+        -> TcMonad theTc (TcTerm theTc, TcType theTc)
+
+
+-- |
+-- Typecheckers that have trace and exception reporting.
+-- 
+class TcHasTrace theTc where
+    -- | Trace TC execution with a helpful message.
+    traceTC :: String -> Text -> TcMonad theTc ()
+    
+    -- | Throw a fatal typechecking exception.
+    throwTC :: TcException theTc -> TcMonad theTc ()
+
+data TcException theTc
+    -- | Mismatch in types: expected one type, got another.
+    = Mismatch      (TcType theTc) (TcType theTc)
+    -- | Couldn't find a point in the TC context.
+    | NotInScope    (TcPoint theTc)
+    -- | Generic failure with an explanatory message.
+    | OtherTcErr    Text
+
+instance Ppr (TcPoint theTc) => Ppr (TcException theTc) where
+    ppr ex = case ex of
+        Mismatch ty1 ty2
+            -> "Mismatch between " <> ppr ty1 <> " and " <> ppr ty2
+        NotInScope x
+            -> "Variable " <> ppr x <> " not in scope"
+        OtherTcErr err
+            -> err
+
+
+-- |
+-- Typecheckers that have meta-variable solvers.
+-- 
+class TcHasMetaVars theTc where
+    -- | Update the typechecker's meta-variable map.
+    modifyMetaVars
+        :: (MetaVarMap theTc -> MetaVarMap theTc)
+        -> TcMonad theTc ()
+    
+    -- | Create a new meta-variable.
+    newMetaVar :: TcMonad theTc MetaVar
+    
+    -- | Make a point from a meta-variable.
+    mkMetaVarPoint :: MetaVar -> TcPoint theTc
+    
+    -- | Try to get the meta-variable from a point.
+    maybeGetMetaVar :: TcPoint theTc -> Maybe MetaVar
+    
+    -- | Write to a meta-variable in the map.
+    writeMetaVar
+        :: MetaVar
+        -> TcTerm theTc
+        -> TcMonad theTc ()
+    
+    -- | Lookup a meta-variable in the map.
+    readMetaVar
+        :: MetaVar
+        -> TcMonad theTc (MetaVarRef theTc)
+
+type MetaVar = Name
+
+type MetaVarMap theTc = Map MetaVar (TcTerm theTc)
+
+data MetaVarRef theTc
+    -- | Unfilled meta-var: can become anything.
+    = Flexible
+    -- | Filled-in meta-var.
+    | Filled    (TcTerm theTc)
+
+
+-- |
+-- Typecheckers that have term typing contexts.
+-- 
+class TcHasContext theTc where
+    -- | Update the typechecker's typing context.
+    modifyContext
+        :: (Context theTc -> Context theTc)
+        -> TcMonad theTc ()
+    
+    -- | Make a point from a variable.
+    mkVarPoint :: ContextVar -> TcPoint theTc
+    
+    -- | Try to get a variable from a point.
+    maybeGetVar :: TcPoint theTc -> Maybe ContextVar
+    
+    -- |
+    -- Create a new variable in the typing context, and pass it to
+    -- a continuation.
+    extendContext
+        :: TcType theTc
+        -> (TcPoint theTc -> TcMonad theTc (TcTerm theTc))
+        -> TcMonad theTc (TcTerm theTc)
+    
+    -- | Get the type of a variable from the typing context.
+    getVarType
+        :: ContextVar
+        -> TcMonad theTc (TcType theTc)
+
+type ContextVar = Name
+
+type Context theTc = Map ContextVar (TcType theTc)
