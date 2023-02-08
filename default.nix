147f2b28f2a66fddc76a64f54ea74efa07999e6e
xand <xand@xandgate.com>
2023-02-07 23:51:37 -0500

    Changed name of default channel to nixpkgs

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 # Package       : Giskard.nix
 # Description   : Build Giskard with Nix-managed dependencies using Cabal-to-Nix
 
-{ nixpkgs ? import <nixos> {} }:
+{ nixpkgs ? import <nixpkgs> {} }:
 
 nixpkgs.haskell.packages.ghc94.callCabal2nix "giskard" ./. {}
 
